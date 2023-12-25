4410dad6273a9d49b1a9220255b950714faa97d4
AleksandarCurcin <aleksandar.curcin@bitsolver.io>
2023-12-25 15:36:15 +0100

    The Python script generates both RTL and TB

---
+++ cordic_tb_generated.v
@@ -0,0 +1,118 @@
+`timescale 1ns/1ps
+
+module cordic_tb_generated();
+
+//constants//
+localparam time  C_CLK_PERIOD   = 2ns;
+localparam time C_RST_PERIOD    = 20ns;
+localparam D_WIDTH             = 16;
+localparam real PI             = 3.141592653589793;
+localparam real ERROR_TOL_R     = 5;
+localparam real ERROR_TOL_A     = 0.05;
+localparam integer MAX_Z_TGT   = 32768;
+
+localparam integer STEP_INT   = 1000;
+
+reg clk;
+reg rst_n;
+integer max_z_tgt;
+integer z_tgt_int;
+integer z_tgt_int_delay;
+integer x_int;
+integer y_int;
+real xc_out_int;
+real yc_out_int;
+real xv_out_int;
+real yv_out_int;
+real error_x_a;
+real error_y_a;
+real error_x_r;
+real error_y_r;
+
+always begin
+    #(0.5*C_CLK_PERIOD);
+    clk <= ~clk;
+end
+
+initial begin
+    clk   <= 1'b0;
+    rst_n <= 1'b0;
+    yv_out_int <= '0;
+    xv_out_int <= '0;
+    #20;
+    rst_n <= 1'b1;
+    #600000;
+    $finish;
+end
+
+initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+end
+
+always @(negedge clk or negedge rst_n) begin
+    if (rst_n == 1'b0) begin
+        z_tgt_int <= -STEP_INT;
+    end
+    else begin
+        z_tgt_int <= z_tgt_int + STEP_INT;
+        z_tgt_int_delay <= #((D_WIDTH-1)*C_CLK_PERIOD) z_tgt_int;
+        if (z_tgt_int >= (MAX_Z_TGT - STEP_INT))
+            z_tgt_int <= '0;
+    end
+end
+
+always @(negedge clk or negedge rst_n) begin
+    if (rst_n == 1'b0) begin
+        error_x_a <= 0;
+        error_y_a <= 0;
+        error_x_r <= 0;
+        error_x_r <= 0;
+    end
+    if ((xc_out_int < 0 || xc_out_int > 1)  && z_tgt_int_delay >= 0) begin
+        $display("Values of x coordinate is not within the range of values corresponding to angles in the first quadrant.");
+         $finish;
+    end
+    if ((yc_out_int < 0 || yc_out_int > 1) && z_tgt_int_delay >= 0) begin
+        $display("Values of y coordinate is not within the range of values corresponding to angles in the first quadrant.");
+         $finish;
+    end
+    if (z_tgt_int_delay >= 0) begin
+        error_x_a  <= xc_out_int - xv_out_int; //apsolute
+        error_y_a  <= yc_out_int - yv_out_int; //apsolute
+        error_x_r  <= (error_x_a/xv_out_int) * 100;  //realative
+        error_y_r  <= (error_y_a/yv_out_int) * 100;  //relative
+    end
+end
+
+always @(*) begin
+    if (error_x_a > ERROR_TOL_A) begin
+        $display("Apsolutna greska x kordinate je veca od %0f%% i iznosi %0f%% za vrednost ugla %0d.", ERROR_TOL_A*100, error_x_a*100,z_tgt_int);
+         $finish;
+    end
+    if (error_y_a > ERROR_TOL_A) begin
+        $display("Apsolutna greska y kordinate je veca od %0f%% i iznosi %0f%% za vrednost ugla %0d.", ERROR_TOL_A*100, error_y_a*100,z_tgt_int);
+         $finish;
+    end
+end
+
+always @(*) begin
+    xc_out_int = $itor(x_int) / (2 ** ((D_WIDTH)-1));
+    yc_out_int = $itor(y_int) / (2 ** ((D_WIDTH)-1));
+    if (z_tgt_int >= (D_WIDTH-1)*(STEP_INT)) begin
+        xv_out_int = $cos(($itor(z_tgt_int_delay) * PI) / (2 ** (D_WIDTH)));
+        yv_out_int = $sin(($itor(z_tgt_int_delay) * PI) / (2 ** (D_WIDTH)));
+    end
+   if (z_tgt_int >= (MAX_Z_TGT - STEP_INT))
+         $finish;
+end
+
+cordic cordic_i (
+    .clk(clk),
+    .rst_n(rst_n),
+    .z_tgt(z_tgt_int),
+    .x_out(x_int),
+    .y_out(y_int)
+);
+
+endmodule
--- gen_cordic.py
+++ gen_cordic.py
@@ -1,5 +1,10 @@
 import numpy as np
 import math as m
+import sys
+import subprocess
+import time
+
+#function for generating cordic (arguments: table of arc tan, signal width in bits, initial value of x coordinate)
 def generate_cordic(atan, data_width, x_init):
   d_w = data_width - 1
   idx = np.zeros(d_w-1)
@@ -22,6 +27,7 @@ def generate_cordic(atan, data_width, x_init):
   cordic+= f"reg signed[{data_width}:0][{data_width}:0] x;\n"
   cordic+= f"reg signed[{data_width}:0][{data_width}:0] y;\n"
   cordic+= f"reg signed[{data_width}:0][{data_width}:0] z;\n"
+  cordic+= f"reg signed[{data_width}:0] z_tmp;\n"
   cordic+= """ 
 always @(posedge clk, negedge rst_n) begin
   if (!rst_n) begin
@@ -37,15 +43,27 @@ always @(posedge clk, negedge rst_n) begin
     cordic += f"    x[{i+1}] <= (!z[{i}][{data_width}]) ? x[{i}] - (y[{i}]>>>{i}) : x[{i}] + (y[{i}]>>>{i});\n"
     cordic += f"    y[{i+1}] <= (!z[{i}][{data_width}]) ? y[{i}] + (x[{i}]>>>{i}) : y[{i}] - (x[{i}]>>>{i});\n"
     cordic += f"    z[{i+1}] <= (!z[{i}][{data_width}]) ? z[{i}] - atan[{(i+1)*(data_width+1)-1}:{i*(data_width+1)}] : z[{i}] + atan[{(i+1)*(data_width+1)-1}:{i*(data_width+1)}];\n"
-
   cordic+= f" end\nend\n"
-  cordic+= f"assign x_out = x[{d_w+1}];\n"
-  cordic+= f"assign y_out = y[{d_w+1}];\n"
+  cordic+= f"assign x_out = (x[{d_w+1}][{data_width}] != 1'b1) ? x[{d_w+1}] : {data_width+1}'d{2**data_width-1};\n"
+  cordic+= f"assign y_out = (y[{d_w+1}][{data_width}] != 1'b1) ? y[{d_w+1}] : {data_width+1}'d{2**data_width-1};\n"
+  #cordic+= f"assign x_out = x[{d_w+1}];\n"
+  #cordic+= f"assign y_out = y[{d_w+1}];\n"
   cordic += "endmodule"
   return cordic
 
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python generate_cordic_tb.py <input_data>")
+        sys.exit(1)
 
-input_width = 10
+    input_width = int(sys.argv[1])
+    error_tol = int(sys.argv[2])
+    step = int(sys.argv[3])
+    wave_req = int(sys.argv[4])
+
+    print(f"Selected input_width: {input_width}")
+    print(f"Selected error tolerance : {error_tol}")
+    print(f"Selected step : {step}")
 pi_half_int = 2**input_width
 artan = np.zeros(input_width)
 artan[0] = m.atan(1)*pi_half_int/np.pi*2
@@ -53,7 +71,6 @@ artan[0] = round(artan[0])
 for i in range (input_width-1):
   artan[i+1] = m.atan(2**(-i-1))*pi_half_int/np.pi*2
   artan[i+1] = round(artan[i+1])
-print(artan,"artan values")
 
 data_width = input_width
 
@@ -63,41 +80,158 @@ x_in = round(x_const*(2**input_width-1))
 print(x_in, "x_in")
 y_in = 0
 
-def cordic_py (x_in, y_in, angle, artan):
-  z_tgt = angle
-  z = 0
-  x = x_in
-  y = y_in
-  for j in range (len(artan)):
-    if (j != 0):
-      x = x_new   
-    x_new = x - (y>>j) if (z < z_tgt) else x + (y>>j)
-    #print(type(x_new))
-    y = (x>>j) + y if (z < z_tgt) else y - (x>>j)
-    z = z - artan[j] if (z >= z_tgt) else z + artan[j]
-    #print(x, y, z)
-  sin = y
-  cos = z
-  return(sin, cos)
-
-
-
-s, c = cordic_py(x_in,y_in,0,artan)
-
-print(s,c,"sin i cos")
-s, c = cordic_py(x_in,y_in,10,artan)
-print(s,c,"sin i cos")
 artan = artan[::-1]
 atan = list(range(input_width))
 for i in range(len(artan)):
   atan[i] = (int(artan[i]))
 print(atan,"artan values")
-# test = float(1)
-# print(test)
-# test = int(test)
-# print(test)
+
 atan = atan[::-1]
 cordic = generate_cordic(atan, data_width,x_in)
 with open('cordic.v', 'w') as f:
     f.write(cordic)
 
+def generate_cordic_tb(input_width,error_tol,step):
+    C_CLK_PERIOD = 2
+    C_RST_PERIOD = 20
+    D_WIDTH = input_width + 1
+    PI = 3.141592653589793
+    ERROR_TOL_R = error_tol
+    ERROR_TOL_A = ERROR_TOL_R/100
+    MAX_Z_TGT = 2 ** (D_WIDTH-1)
+
+    with open("cordic_tb_generated.v", "w") as file:
+        file.write("`timescale 1ns/1ps\n\n")
+        file.write("module cordic_tb_generated();\n\n")
+        file.write("//constants//\n")
+        file.write(f"localparam time  C_CLK_PERIOD   = 2ns;\n")
+        file.write(f"localparam time C_RST_PERIOD    = 20ns;\n")
+        file.write(f"localparam D_WIDTH             = {D_WIDTH};\n")
+        file.write(f"localparam real PI             = 3.141592653589793;\n")
+        file.write(f"localparam real ERROR_TOL_R     = {ERROR_TOL_R};\n")
+        file.write(f"localparam real ERROR_TOL_A     = {ERROR_TOL_A};\n")
+        file.write(f"localparam integer MAX_Z_TGT   = {MAX_Z_TGT};\n\n")
+        file.write(f"localparam integer STEP_INT   = {step};\n\n")
+        # Declare signals
+        file.write("reg clk;\n")
+        file.write("reg rst_n;\n")
+        file.write("integer max_z_tgt;\n")
+        file.write("integer z_tgt_int;\n")
+        file.write("integer z_tgt_int_delay;\n")
+        file.write("integer x_int;\n")
+        file.write("integer y_int;\n")
+        file.write("real xc_out_int;\n")
+        file.write("real yc_out_int;\n")
+        file.write("real xv_out_int;\n")
+        file.write("real yv_out_int;\n")
+        file.write("real error_x_a;\n")
+        file.write("real error_y_a;\n")
+        file.write("real error_x_r;\n")
+        file.write("real error_y_r;\n\n")
+
+        # Always block for clock generation
+        file.write("always begin\n")
+        file.write("    #(0.5*C_CLK_PERIOD);\n")
+        file.write("    clk <= ~clk;\n")
+        file.write("end\n\n")
+
+        # Initial block for reset and finish
+        file.write("initial begin\n")
+        file.write("    clk   <= 1'b0;\n")
+        file.write("    rst_n <= 1'b0;\n")
+        file.write("    yv_out_int <= '0;\n")
+        file.write("    xv_out_int <= '0;\n")
+        file.write(f"    #{C_RST_PERIOD};\n")
+        file.write("    rst_n <= 1'b1;\n")
+        file.write(f"    #{30000*C_RST_PERIOD};\n")
+        file.write("    $finish;\n")
+        file.write("end\n\n")
+
+        # Initial block for waveform dumping
+        file.write("initial begin\n")
+        file.write("    $dumpfile(\"waves.vcd\");\n")
+        file.write("    $dumpvars;\n")
+        file.write("end\n\n")
+
+        # Always block for z_tgt_int increment
+        file.write("always @(negedge clk or negedge rst_n) begin\n")
+        file.write("    if (rst_n == 1'b0) begin\n")
+        file.write("        z_tgt_int <= -STEP_INT;\n")
+        file.write("    end\n")
+        file.write("    else begin\n")
+        file.write("        z_tgt_int <= z_tgt_int + STEP_INT;\n")
+        file.write("        z_tgt_int_delay <= #((D_WIDTH-1)*C_CLK_PERIOD) z_tgt_int;\n")
+        file.write(f"        if (z_tgt_int >= (MAX_Z_TGT - STEP_INT))\n")
+        file.write("            z_tgt_int <= '0;\n")
+        file.write("    end\n")
+        file.write("end\n\n")
+
+        # Always block for z_tgt_int increment
+        file.write("always @(negedge clk or negedge rst_n) begin\n")
+        file.write("    if (rst_n == 1'b0) begin\n")
+        file.write("        error_x_a <= 0;\n")
+        file.write("        error_y_a <= 0;\n")
+        file.write("        error_x_r <= 0;\n")
+        file.write("        error_x_r <= 0;\n")
+        file.write("    end\n")
+        file.write(f"    if ((xc_out_int < 0 || xc_out_int > 1)  && z_tgt_int_delay >= 0) begin\n")
+        file.write(f"        $display(\"Values of x coordinate is not within the range of values corresponding to angles in the first quadrant.\");\n")
+        file.write("         $finish;\n")
+        file.write("    end\n")
+        file.write(f"    if ((yc_out_int < 0 || yc_out_int > 1) && z_tgt_int_delay >= 0) begin\n")
+        file.write(f"        $display(\"Values of y coordinate is not within the range of values corresponding to angles in the first quadrant.\");\n")
+        file.write("         $finish;\n")
+        file.write("    end\n")
+        file.write("    if (z_tgt_int_delay >= 0) begin\n")
+        file.write("        error_x_a  <= xc_out_int - xv_out_int; //apsolute\n")
+        file.write("        error_y_a  <= yc_out_int - yv_out_int; //apsolute\n")
+        file.write("        error_x_r  <= (error_x_a/xv_out_int) * 100;  //realative\n")
+        file.write("        error_y_r  <= (error_y_a/yv_out_int) * 100;  //relative\n")
+        file.write("    end\n")
+        file.write("end\n\n")
+
+
+        # Always block for error display
+        file.write("always @(*) begin\n")
+        file.write(f"    if (error_x_a > ERROR_TOL_A) begin\n")
+        file.write(f"        $display(\"Apsolutna greska x kordinate je veca od %0f%% i iznosi %0f%% za vrednost ugla %0d.\", ERROR_TOL_A*100, error_x_a*100,z_tgt_int);\n")
+        file.write("         $finish;\n")
+        file.write("    end\n")
+        file.write(f"    if (error_y_a > ERROR_TOL_A) begin\n")
+        file.write(f"        $display(\"Apsolutna greska y kordinate je veca od %0f%% i iznosi %0f%% za vrednost ugla %0d.\", ERROR_TOL_A*100, error_y_a*100,z_tgt_int);\n")
+        file.write("         $finish;\n")
+        file.write("    end\n")
+        file.write("end\n\n")
+
+        # Always block for coordinate calculations
+        file.write("always @(*) begin\n")
+        file.write("    xc_out_int = $itor(x_int) / (2 ** ((D_WIDTH)-1));\n")
+        file.write("    yc_out_int = $itor(y_int) / (2 ** ((D_WIDTH)-1));\n")
+        file.write("    if (z_tgt_int >= (D_WIDTH-1)*(STEP_INT)) begin\n")
+        file.write(f"        xv_out_int = $cos(($itor(z_tgt_int_delay) * PI) / (2 ** (D_WIDTH)));\n")
+        file.write(f"        yv_out_int = $sin(($itor(z_tgt_int_delay) * PI) / (2 ** (D_WIDTH)));\n")
+        file.write("    end\n")
+        file.write(f"   if (z_tgt_int >= (MAX_Z_TGT - STEP_INT))\n")
+        file.write("         $finish;\n")
+        file.write("end\n\n")
+
+      
+
+        # Module instantiation
+        file.write("cordic cordic_i (\n")
+        file.write("    .clk(clk),\n")
+        file.write("    .rst_n(rst_n),\n")
+        file.write("    .z_tgt(z_tgt_int),\n")
+        file.write("    .x_out(x_int),\n")
+        file.write("    .y_out(y_int)\n")
+        file.write(");\n\n")
+        
+        file.write("endmodule\n")
+
+generate_cordic_tb(input_width,error_tol, step)
+
+time.sleep(0.5)
+subprocess.run (["iverilog", "-g2012", "cordic.v", "cordic_tb_generated.v"])
+subprocess.run (["vvp", "a.out"])
+if (wave_req >= 1):
+  subprocess.run (["gtkwave", "waves.vcd"])
